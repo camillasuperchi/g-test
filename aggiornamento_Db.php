818d67f52a9da6bf61d93ecdcfb460cd72703a95
camilla superchi <camilla.superchi@gmail.com>
2018-08-23 18:32:36 +0200

    Commit1

---
+++ aggiornamento_Db.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Introduzione alla poetica di Foscolo</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>Introduzione alla poetica di Foscolo</h1>
+  <p>Resize this responsive page to see the effect!</p> 
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Column 1</h3>
+      <p
+.</p>
+      <p>L suo titanismo di stampo romantico si stemperano in immagini serene di compostezza neoclassica, secondo l'insegnamento del Winckelmann.[3]
+
+Tornato per breve tempo a vivere stabilmente in Italia e nel Lombardo-Veneto (allora ancora parte del Regno d'Italia filofrancese) nel 1813, partì presto in un nuovo volontario esilio e morì povero qualche anno dopo a Londra, nel sobborgo di Turnham Green. Dopo l'Unità, nel 1871, le sue ceneri furono riportate per decreto del governo italiano in patria e inumate nella Basilica di Santa Croce a Firenze, il Tempio dell'Itale Glorie da lui cantato nei Sepolcri (1807). </p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 2</h3>
+      <p>
+Costretto fin da giovane ad allontanarsi dalla sua patria (l'isola greca di Zacinto/Zákynthos, oggi nota in italiano come Zante), allora territorio della Repubblica di Veneziar tutta la vita,ui era nato e cresciuto, tramite la sua formazione letteraria e il legame con</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 3</h3>        
+      <p>Lorem ipsumicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+  </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h3>Popover Example</h3>
+  <p>Still hoping that his country would be freed by Napoleon, he served as a volunteer in the French army, took part in the battle of the Trebbia and the siege of Genoa, was wounded and made prisoner. When released he returned to Milan, and there gave the last touches to his Ortis, published a translation of and commentary upon Callimachus, commenced a version of the Iliad and began his translation of Laurence Sterne's Sentimental Journey. He also took part in a failed memorandum intended to present a new model of unified Italian government to Napoleon. </p>
+  <a href="#" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover">Toggle popover</a>
+</div>
+
+<script>
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();   
+});
+</script>
+
+</body>
+</html>
+
+  
+</div>
+
+</body>
+</html>
